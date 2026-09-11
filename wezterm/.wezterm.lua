e299e225184dcbcee7a5167c88409667ac1986b1
Christopher Britton <christopher.l.britton@outlook.com>
2026-09-11 11:45:44 -0400

    Fix shift key producing no input in WSL by disabling the kitty keyboard protocol

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,10 +10,11 @@ return {
   color_scheme = "rose-pine",
   window_background_opacity = 0.9,
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-  enable_kitty_keyboard = true,
   keys = {
     -- Make Ctrl+Enter distinguishable from Enter by sending a CSI-u sequence
-    -- that Neovim parses as <C-CR>. Belt-and-braces alongside kitty keyboard.
+    -- that Neovim parses as <C-CR>. Do not re-enable enable_kitty_keyboard for this:
+    -- a TUI that exits without popping the protocol stack leaves shifted keys
+    -- emitting CSI-u, which readline and plain WSL shells do not parse.
     { key = 'Enter', mods = 'CTRL', action = act.SendString('\x1b[13;5u') },
   },
 }
